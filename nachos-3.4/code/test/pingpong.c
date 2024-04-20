d39bd1ac446eeb6ffc3a3deca49567d06f623647
KietHuynh <kiethuynh243@MacBook-Air-2.local>
2024-04-20 10:25:55 +0700

    feat: add pingpong

---
+++ nachos-3.4/code/test/pingpong.c
@@ -0,0 +1,11 @@
+#include "syscall.h"
+
+void main() {
+    SpaceId PingID, PongID;
+    PrintString("PingPong\n");
+    PingID = Exec("../test/ping");
+    PongID = Exec("../test/pong");
+    Join(PingID);
+    Join(PongID);
+    Exit(0);
+}
